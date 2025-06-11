cb29da5b4a74ab4877531788aa2311f1ef3b38bf
Toewaioo <sawtoewaioo2003@gmail.com>
2025-06-11 07:25:25 +0630

    fixed bug

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,22 @@ use App\Http\Middleware\ApiTokenAuth;
 use App\Http\Middleware\VipAccess;
 use App\Http\Controllers\Api\ContentController;
 use App\Http\Controllers\Api\SubscriptionController;
+use Illuminate\Support\Facades\DB;
 /*--------------------------------------------------------------------------
 | Public Routes
 |--------------------------------------------------------------------------*/
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/upgrade-info', [ContentController::class, 'upgradeInfo']);
+Route::get('/db-con', function () {
+    try {
+        $dbconnect = DB::connection()->getPDO();
+        $dbname = DB::connection()->getDatabaseName();
+        echo "Connected successfully to the database. Database name is :" . $dbname;
+    } catch (Exception $e) {
+        echo "Error in connecting to the database" . $e->getMessage();
+    }
+});
 
 /*--------------------------------------------------------------------------
 | Authenticated Routes (API Token Required)
@@ -23,11 +34,11 @@ Route::middleware([ApiTokenAuth::class])->group(function () {
     Route::get('/contents', [ContentController::class, 'listContents']);
     Route::get('/normal-contents', [ContentController::class, 'normalContents']);
     Route::get('/contents/{id}', [ContentController::class, 'getContentDetails']);
-    
+
     // Subscription Management
     Route::post('/verify-key', [SubscriptionController::class, 'verifyKey']);
     Route::get('/subscription-status', [SubscriptionController::class, 'subscriptionStatus']);
-    
+
     /*----------------------------------------------------------------------
     | VIP-Only Routes (VIP Subscription Required)
     |----------------------------------------------------------------------*/
@@ -44,12 +55,12 @@ Route::prefix('admin')->group(function () {
     // VIP Management
     Route::post('/upgrade', [AdminController::class, 'upgradeToVip']);
     Route::post('/create-key', [AdminController::class, 'createSubscriptionKey']);
-    
+
     // Content Management
     Route::post('/contents', [AdminController::class, 'createContent']);
     Route::put('/contents/{id}', [AdminController::class, 'updateContent']);
     Route::delete('/contents/{id}', [AdminController::class, 'deleteContent']);
-    
+
     // Subscription Management
     Route::get('/subscriptions', [AdminController::class, 'listSubscriptions']);
     Route::put('/subscriptions/{id}/deactivate', [AdminController::class, 'deactivateSubscription']);
